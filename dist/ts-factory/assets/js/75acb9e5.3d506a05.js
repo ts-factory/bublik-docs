536e5b7071f0ccb468afdde3d98da131d9ca0011
okt-limonikas <okt-limonikas@users.noreply.github.com>
2025-12-18 12:29:23 +0000

    Release: v2.6.0 (SHA: 7ddb2a3b415fddd82184c2037cf7d29b609561e8)

--- dist/ts-factory/assets/js/75acb9e5.3d506a05.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkbublik_release=self.webpackChunkbublik_release||[]).push([[506],{7777:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/bublik/docs/blog/page/4","page":4,"postsPerPage":10,"totalPages":4,"totalCount":40,"previousPage":"/bublik/docs/blog/page/3","blogDescription":"The personal blog of Bublik","blogTitle":"Bublik Blog"}}')}}]);
